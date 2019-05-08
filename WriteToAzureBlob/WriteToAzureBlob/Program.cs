96f979d332ef3beec995517b86d98493a36d6fb3
alokbhatnagar0580 <49979380+alokbhatnagar0580@users.noreply.github.com>
2019-05-08 11:53:44 +0530

    Azure Blob PoC

---
+++ WriteToAzureBlob/WriteToAzureBlob/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System.IO;
+
+namespace WriteToAzureBlob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateBlobAysnc().GetAwaiter().GetResult();
+        }
+
+        static async Task CreateBlobAysnc()
+        {
+            string storageConnection = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+
+            //create a block blob 
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            //create a container 
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("testalok11");
+
+            //create a container if it is not already exists
+
+            if (await cloudBlobContainer.CreateIfNotExistsAsync())
+            {
+
+                await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            }
+
+            // Create a file in your local MyDocuments folder to upload to a blob.
+            string localPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string localFileName = "QuickStart_" + Guid.NewGuid().ToString() + ".txt";
+            string sourceFile = Path.Combine(localPath, localFileName);
+            // Write text to the file.
+            File.WriteAllText(sourceFile, "Hello, World!");
+            //get Blob reference
+
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(localFileName);
+            await cloudBlockBlob.UploadFromFileAsync(sourceFile);
+        }
+    }
+}
